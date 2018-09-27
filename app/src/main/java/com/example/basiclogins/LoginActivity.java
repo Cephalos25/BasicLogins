4b02c44217dfd773da04fe842b361002123ad019
Isaiah <tscephalos@gmail.com>
2018-09-27 08:52:24 -0700

    Added buttons to create account page, started wiring

--- app/src/main/java/com/example/basiclogins/LoginActivity.java
+++ app/src/main/java/com/example/basiclogins/LoginActivity.java
@@ -2,12 +2,29 @@ package com.example.basiclogins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText fieldUsername;
+    private EditText fieldPassword;
+    private Button buttonLogin;
+    private TextView textViewCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        wireWidgets();
+    }
+
+    private void wireWidgets() {
+        fieldUsername = findViewById(R.id.editText_login_username);
+        fieldPassword = findViewById(R.id.editText_login_password);
+        buttonLogin = findViewById(R.id.button_login_login);
+        textViewCreateAccount = findViewById(R.id.textView_login_createaccount);
     }
 }
