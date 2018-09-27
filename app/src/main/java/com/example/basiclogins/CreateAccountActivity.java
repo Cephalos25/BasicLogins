a1a6f50ef339c79fa2059951788f7c6865ecf397
Isaiah <tscephalos@gmail.com>
2018-09-27 09:39:53 -0700

    OnClickListener added to create account button. Screen flashes red if account creation is unsuccessful. Account storage system not yet added.

--- app/src/main/java/com/example/basiclogins/CreateAccountActivity.java
+++ app/src/main/java/com/example/basiclogins/CreateAccountActivity.java
@@ -1,8 +1,12 @@
 package com.example.basiclogins;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,6 +18,7 @@ public class CreateAccountActivity extends AppCompatActivity {
     private EditText confirmpasswordField;
     private EditText emailField;
     private Button buttonCreateAccount;
+    private ConstraintLayout bgelement;
 
     private String name;
     private String username;
@@ -29,6 +34,42 @@ public class CreateAccountActivity extends AppCompatActivity {
         usernameField.setText(username);
 
         wireWidgets();
+        buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = nameField.getText().toString();
+                username = usernameField.getText().toString();
+                password = passwordField.getText().toString();
+                email = emailField.getText().toString();
+                if(password.length()<6 || password.length()>12){
+                    bgelement.setBackgroundColor(Color.argb(255,255,0,0));
+                    new CountDownTimer(1000,1000){
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+
+                        }
+                        @Override
+                        public void onFinish() {
+                            bgelement.setBackgroundColor(Color.argb(255,255,255,255));
+                        }
+                    }.start();
+                } else if (password!=confirmpasswordField.getText().toString()){
+                    bgelement.setBackgroundColor(Color.argb(255,255,0,0));
+                    new CountDownTimer(1000,1000){
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+
+                        }
+                        @Override
+                        public void onFinish() {
+                            bgelement.setBackgroundColor(Color.argb(255,255,255,255));
+                        }
+                    }.start();
+                } else {
+
+                }
+            }
+        });
     }
 
     private void wireWidgets() {
@@ -38,5 +79,6 @@ public class CreateAccountActivity extends AppCompatActivity {
         confirmpasswordField = findViewById(R.id.editText_createaccount_confirmpassword);
         emailField = findViewById(R.id.editText_createaccount_email);
         buttonCreateAccount = findViewById(R.id.button_createaccount_create);
+        bgelement = findViewById(R.id.createaccountlayout);
     }
 }
